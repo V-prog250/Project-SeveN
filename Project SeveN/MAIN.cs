4c243a9cdb86de12f11aee9c927033046a5d1132
angel1wladi1 <sseevveennaw@gmail.com>
2023-03-12 10:21:42 +0200

    newbuton

--- Project SeveN/MAIN.cs
+++ Project SeveN/MAIN.cs
@@ -16,5 +16,10 @@ namespace Project_SeveN
         {
             InitializeComponent();
         }
+
+        private void gunaAdvenceTileButton1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
