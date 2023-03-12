548663eecadc4c038fdf1a5db8b4dd936067825d
angel1wladi1 <sseevveennaw@gmail.com>
2023-03-12 10:09:46 +0200

    da

--- Project SeveN/Loader.Designer.cs
+++ Project SeveN/Loader.Designer.cs
@@ -31,13 +31,14 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loader));
             this.pnlTop = new Guna.UI.WinForms.GunaGradient2Panel();
-            this.btnClose = new Guna.UI.WinForms.GunaAdvenceTileButton();
-            this.btnMInimize = new Guna.UI.WinForms.GunaAdvenceTileButton();
-            this.drgctrlpaneltop = new Guna.UI.WinForms.GunaDragControl(this.components);
             this.lblName = new Guna.UI.WinForms.GunaLabel();
+            this.btnMInimize = new Guna.UI.WinForms.GunaAdvenceTileButton();
+            this.btnClose = new Guna.UI.WinForms.GunaAdvenceTileButton();
+            this.drgctrlpaneltop = new Guna.UI.WinForms.GunaDragControl(this.components);
             this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.pnlTop.SuspendLayout();
             this.gunaPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -56,36 +57,16 @@
             this.pnlTop.Size = new System.Drawing.Size(722, 40);
             this.pnlTop.TabIndex = 0;
             // 
-            // btnClose
+            // lblName
             // 
-            this.btnClose.AnimationHoverSpeed = 0.07F;
-            this.btnClose.AnimationSpeed = 0.03F;
-            this.btnClose.BaseColor = System.Drawing.Color.Transparent;
-            this.btnClose.BorderColor = System.Drawing.Color.Black;
-            this.btnClose.CheckedBaseColor = System.Drawing.Color.Gray;
-            this.btnClose.CheckedBorderColor = System.Drawing.Color.Black;
-            this.btnClose.CheckedForeColor = System.Drawing.Color.White;
-            this.btnClose.CheckedImage = ((System.Drawing.Image)(resources.GetObject("btnClose.CheckedImage")));
-            this.btnClose.CheckedLineColor = System.Drawing.Color.DimGray;
-            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.btnClose.FocusedColor = System.Drawing.Color.Empty;
-            this.btnClose.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Image = null;
-            this.btnClose.ImageSize = new System.Drawing.Size(52, 52);
-            this.btnClose.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(58)))), ((int)(((byte)(170)))));
-            this.btnClose.Location = new System.Drawing.Point(682, 3);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.OnHoverBaseColor = System.Drawing.Color.Red;
-            this.btnClose.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.btnClose.OnHoverForeColor = System.Drawing.Color.White;
-            this.btnClose.OnHoverImage = null;
-            this.btnClose.OnHoverLineColor = System.Drawing.Color.Transparent;
-            this.btnClose.OnPressedColor = System.Drawing.Color.Black;
-            this.btnClose.Size = new System.Drawing.Size(37, 37);
-            this.btnClose.TabIndex = 1;
-            this.btnClose.Text = "X";
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblName.Location = new System.Drawing.Point(154, -1);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(404, 40);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Project SeveN Loader";
             // 
             // btnMInimize
             // 
@@ -118,20 +99,40 @@
             this.btnMInimize.Text = "_";
             this.btnMInimize.Click += new System.EventHandler(this.btnMInimize_Click);
             // 
+            // btnClose
+            // 
+            this.btnClose.AnimationHoverSpeed = 0.07F;
+            this.btnClose.AnimationSpeed = 0.03F;
+            this.btnClose.BaseColor = System.Drawing.Color.Transparent;
+            this.btnClose.BorderColor = System.Drawing.Color.Black;
+            this.btnClose.CheckedBaseColor = System.Drawing.Color.Gray;
+            this.btnClose.CheckedBorderColor = System.Drawing.Color.Black;
+            this.btnClose.CheckedForeColor = System.Drawing.Color.White;
+            this.btnClose.CheckedImage = ((System.Drawing.Image)(resources.GetObject("btnClose.CheckedImage")));
+            this.btnClose.CheckedLineColor = System.Drawing.Color.DimGray;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnClose.FocusedColor = System.Drawing.Color.Empty;
+            this.btnClose.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Image = null;
+            this.btnClose.ImageSize = new System.Drawing.Size(52, 52);
+            this.btnClose.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(58)))), ((int)(((byte)(170)))));
+            this.btnClose.Location = new System.Drawing.Point(682, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.OnHoverBaseColor = System.Drawing.Color.Red;
+            this.btnClose.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnClose.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnClose.OnHoverImage = null;
+            this.btnClose.OnHoverLineColor = System.Drawing.Color.Transparent;
+            this.btnClose.OnPressedColor = System.Drawing.Color.Black;
+            this.btnClose.Size = new System.Drawing.Size(37, 37);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
             // drgctrlpaneltop
             // 
             this.drgctrlpaneltop.TargetControl = this.pnlTop;
-            // 
-            // lblName
-            // 
-            this.lblName.AutoSize = true;
-            this.lblName.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblName.Location = new System.Drawing.Point(154, -1);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(404, 40);
-            this.lblName.TabIndex = 1;
-            this.lblName.Text = "Project SeveN Loader";
             // 
             // gunaPanel1
             // 
@@ -156,12 +157,22 @@
             this.timer1.Interval = 15;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Loader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(716, 375);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.gunaPanel1);
             this.Controls.Add(this.pnlTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -186,6 +197,7 @@
         private Guna.UI.WinForms.GunaPanel gunaPanel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
