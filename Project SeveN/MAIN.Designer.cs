89db53eb3ba8b8524fbc6151c9bc25f8e318c8d0
angel1wladi1 <sseevveennaw@gmail.com>
2023-03-12 10:22:56 +0200

    Test

--- Project SeveN/MAIN.Designer.cs
+++ Project SeveN/MAIN.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MAIN));
             this.gunaAdvenceTileButton1 = new Guna.UI.WinForms.GunaAdvenceTileButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // gunaAdvenceTileButton1
@@ -63,20 +64,33 @@
             this.gunaAdvenceTileButton1.Text = "gunaAdvenceTileButton1";
             this.gunaAdvenceTileButton1.Click += new System.EventHandler(this.gunaAdvenceTileButton1_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(249, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // MAIN
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.gunaAdvenceTileButton1);
             this.Name = "MAIN";
             this.Text = "MAIN";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Guna.UI.WinForms.GunaAdvenceTileButton gunaAdvenceTileButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
